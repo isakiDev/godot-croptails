c484d3a3341bc3a4c7f0f1437cde4614c6a63ba8
isakidev <gaspar.c.developer@gmail.com>
2025-11-24 01:58:19 -0300

    feat: :sparkles: add plants and tilled dirt

--- hurt_component.gd
+++ hurt_component.gd
@@ -8,8 +8,8 @@ signal hurt
 func _on_area_entered(area: Area2D) -> void:
 	var hit_component = area as HitComponent
 	
-	print(tool)
-	print(hit_component.current_tool)
+	#print(tool)
+	#print(hit_component.current_tool)
 	
 	if tool == hit_component.current_tool:
 		hurt.emit(hit_component.hit_damage)
